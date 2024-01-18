327780190caecb41a805e7394549c571c791fe29
Diego Fernandes <didioliveira824@gmail.com>
2024-01-18 14:04:12 -0300

    update: configs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/quest-react-base/",
   plugins: [react()],
+  base: "/quest-react-base/",
 });
